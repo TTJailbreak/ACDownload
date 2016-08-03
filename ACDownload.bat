44e3f89f975855dde77dfd5ff72ac01f9d520b11
TTJailbreak <nullerror40404@gmail.com>
2016-08-03 14:59:52 -0700

    Add files via upload

---
+++ ACDownload.bat
@@ -0,0 +1,68 @@
+@echo off
+:Start2
+cls
+goto Start
+:Start
+title ACDownloader
+echo Welcome to ACDownloader
+echo Please select a mod to install
+echo ----------------------------------------­-----------------------
+echo 1) Meat Mod
+echo 2) Color Mod
+echo 3) Electric Mod
+echo 4) All
+echo Input your choice
+set input=
+set /p input= Choice:
+if %input%==1 goto A if NOT goto Start2
+if %input%==2 goto B if NOT goto Start2
+if %input%==3 goto C if NOT goto Start2
+if %input%==4 goto D if NOT goto Start2
+:A
+cls
+echo File Downloaded
+echo Now choose what you want to do.
+echo 1) Go back to the beginning
+echo 2) Exit
+set input=
+set /p input= Choice:
+if %input%==1 goto Start2 if NOT goto Start 2
+if %input%==2 goto Exit if NOT goto Start 2
+:Exit
+exit
+:B
+cls
+echo File Downloaded
+echo Now choose what you want to do.
+echo 1) Go back to the beginning
+echo 2) Exit
+set input=
+set /p input= Choice:
+if %input%==1 goto Start2 if NOT goto Start 2
+if %input%==2 goto Exit if NOT goto Start 2
+:Exit
+exit
+:C
+cls
+echo File Downloaded
+echo Now choose what you want to do.
+echo 1) Go back to the beginning
+echo 2) Exit
+set input=
+set /p input= Choice:
+if %input%==1 goto Start2 if NOT goto Start 2
+if %input%==2 goto Exit if NOT goto Start 2
+:Exit
+exit
+:D
+cls
+echo File Downloaded
+echo Now choose what you want to do.
+echo 1) Go back to the beginning
+echo 2) Exit
+set input=
+set /p input= Choice:
+if %input%==1 goto Start2 if NOT goto Start 2
+if %input%==2 goto Exit if NOT goto Start 2
+:Exit
+exit
